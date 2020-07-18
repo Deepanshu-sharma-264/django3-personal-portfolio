6f2b707deeb75cec718394e399072dc41f0ea6fc
Deepanshu Sharma <deepanshusharma12345.me@gmail.com>
2020-07-18 22:46:41 +0530

    My new commit!

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,4 +6,4 @@ class Project(models.Model):
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
     def __str__(self):
-        return self.title
+        return self.title        
